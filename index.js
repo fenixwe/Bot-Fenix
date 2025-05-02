e6381597fad32962dbedcebe022b2d380e4c21fa
fenixwe <perdonerdon@gmail.com>
2025-05-01 19:51:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ client.on('ready', () => {
   client.user.setPresence({
     status: 'online', 
     activities: [{
-      name: 'CubeRaze.aternos.me',
+      name: 'creado por fandesoycuboogg',
       type: 4 
     }]
   });
